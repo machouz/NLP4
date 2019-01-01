ae72febf090bf1020d23aeb5681d15b7f168b986
arie.cattan <arie.cattan@gmail.com>
2019-01-01 18:02:40 +0200

    utils + eval

--- src/eval.py
+++ src/eval.py
@@ -3,10 +3,29 @@ from utils import *
 import sys
 
 def get_precision(gold, pred):
-    TP = 0
+    TP = 0.0
+    FP = 0.0
+
+    for pr in pred:
+        if pr in gold:
+            TP += 1
+        else:
+            FP += 1
+
+    return TP / (TP + FP)
+
 
 def get_recall(gold, pred):
-    precision = 0.0
+    TP = 0.0
+    FN = 0.0
+
+    for pr in gold:
+        if pr in pred:
+            TP += 1
+        else:
+            FN += 1
+
+    return TP / (TP + FN)
 
 def get_F1(recall, precision):
     return 2*(recall * precision) / (recall + precision)
@@ -16,3 +35,14 @@ if __name__ == '__main__':
     gold_file = sys.argv[1] if len(sys.argv) > 1 else '../data/Annotation/DEV.annotations.txt'
     pred_file = sys.argv[2] if len(sys.argv) > 2 else '../data/Annotation/DEV.annotations.txt'
 
+
+    gold = read_annotations_file(gold_file)
+    pred = read_annotations_file(gold_file)
+
+    precision = get_precision(gold, pred)
+    recall = get_recall(gold, pred)
+    F1 = get_F1(recall, precision)
+
+    print("Precision: {}".format(precision))
+    print("Recall: {}".format(recall))
+    print("F1: {}".format(F1))
--- src/utils.py
+++ src/utils.py
@@ -5,12 +5,13 @@ def read_annotations_file(fname):
     output = []
     with open(fname, 'r') as f:
         for line in f:
-            sent, per, _, loc, _ = line.split('\t')
-            output.append([sent, per, loc])
+            sent, per, relation, loc, _ = line.split('\t')
+            if relation == 'Live_In':
+                output.append(np.array([sent, per, loc]))
 
-    return output
+    return np.array(output)
 
 
 if __name__ == '__main__':
     fname = '../data/Annotation/DEV.annotations.txt'
-    out = read_annoations_file(fname)
+    out = read_annotations_file(fname)
