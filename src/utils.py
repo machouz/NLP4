d819e6d59fe0e9adf844d47d4f43810ad6b43740
machou <uzanmacho@gmail.com>
2019-01-01 18:07:23 +0200

    add processing functions

--- src/utils.py
+++ src/utils.py
@@ -11,6 +11,33 @@ def read_annotations_file(fname):
     return output
 
 
+def split_processed_file(fname):
+    processed = []
+    for line in file(fname):
+        splitted = line.split()
+        if len(splitted) > 1 and splitted[0] == '#id:':
+            processed.append([])
+        processed[-1].append(line[:-1])  # remove \n from line
+    return processed
+
+
+def sentence2dic(processed_sentence):
+    sentence = []
+    for word in processed_sentence:
+        ID, TEXT, LEMMA, TAG, POS, HEAD, DEP, IOB, TYPE = word.split("\t")
+        dict = {"ID": ID, "TEXT": TEXT, "LEMMA": LEMMA, "TAG": TAG, "POS": POS, "HEAD": HEAD, "DEP": DEP, "IOB": IOB,
+                "TYPE": TYPE}
+        sentence.append(dict)
+    return sentence
+
+
+def read_processed_file(fname):
+    lines = split_processed_file(fname)
+    lines = [processed[2:-1] for processed in lines]  # remove two first comment and last \n)
+    lines = [sentence2dic(sentence) for sentence in lines]  # remove two first comment and last \n)
+    return lines
+
+
 if __name__ == '__main__':
-    fname = '../data/Annotation/DEV.annotations.txt'
-    out = read_annoations_file(fname)
+    fname = 'data/Processed_Corpus/Corpus.DEV.processed.txt'
+    out = read_processed_file(fname)
