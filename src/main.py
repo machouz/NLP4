9309e54d2562a86dee54e27932dad5ba9f5d4ad7
arie.cattan <arie.cattan@gmail.com>
2019-01-17 21:07:54 +0200

    extract

--- src/main.py
+++ src/main.py
@@ -16,14 +16,21 @@ def feature_convert(features_dic):
 
 
 input_file = sys.argv[1] if len(sys.argv) > 1 else 'data/Processed_Corpus/Corpus.TRAIN.processed.txt'
-model_file = sys.argv[2] if len(sys.argv) > 2 else 'model'
-feature_map_file = sys.argv[3] if len(sys.argv) > 3 else 'feature_map_file'
+model_file = 'model'
+feature_map_file = 'feature_map_file'
 output_file = sys.argv[4] if len(sys.argv) > 4 else 'data/Annotation/output_model_train.txt'
 
 model = pickle.load(open(model_file, 'rb'))
 features2id = file_to_dic(feature_map_file)
 id2features = {v: k for k, v in features2id.items()}
 zero_features_vector = np.zeros(max(features2id.values()))
+
+
+with open(input_file, 'r') as f:
+    first_line = f.readline()
+
+
+
 
 data = read_processed_file(input_file)
 
